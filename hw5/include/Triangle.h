e543a1c66b17efb62b602d7181f5886ecb9ef9c3
Miguel-S-V <45907688+Miguel-S-V@users.noreply.github.com>
2020-04-28 17:32:52 -0500

    Update Triangle.h

--- hw5/include/Triangle.h
+++ hw5/include/Triangle.h
@@ -3,20 +3,26 @@
 
 #include "Shape.h"
 
-//adapted from https://github.com/cemuyuk/RayTracing
+
+
+
+//SOURCE https://github.com/cemuyuk/RayTracing
 
 namespace Raytracer148 {
-    class Triangle : public Shape {
+    class Triangle : public Shape { //setup for Triangle shape dervies from Shape class
     public:
+        //Constructor for Triangle
         Triangle(Eigen::Vector3d &vertex0, Eigen::Vector3d &vertex1, Eigen::Vector3d &vertex2, Eigen::Vector3d &kamb, Eigen::Vector3d &kdif, Eigen::Vector3d &kspec) :
             a(vertex0),b(vertex1),c(vertex2)
         {
+                //initialize vars
             ka=kamb;
             kd=kdif;
             ks=kspec;
         }
         virtual HitRecord intersect (const Ray &ray);
     private:
+        //private variables
         Eigen::Vector3d a;
         Eigen::Vector3d b;
         Eigen::Vector3d c;
