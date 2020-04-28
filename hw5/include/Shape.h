1ce81e55c87cd45a1e4fd8dcbefb05ce2275261f
Miguel-S-V <45907688+Miguel-S-V@users.noreply.github.com>
2020-04-28 17:31:05 -0500

    Update Shape.h

--- hw5/include/Shape.h
+++ hw5/include/Shape.h
@@ -3,14 +3,20 @@
 
 #include <Eigen/Dense>
 
-//adapted from https://github.com/cemuyuk/RayTracing
 
+
+//SOURCE  https://github.com/cemuyuk/RayTracing
+
+
+//namespace
 namespace Raytracer148 {
 	struct Ray {
+		//Eigen vars	
 		Eigen::Vector3d origin, direction;
 	};
 	class Shape;
 	struct HitRecord {
+		
 		Eigen::Vector3d position, normal;
 		double t;
 		Shape* sh=nullptr;
@@ -18,8 +24,11 @@ namespace Raytracer148 {
 
 	class Shape {
 	public:
+		
 		Eigen::Vector3d ka,kd,ks;
+		
 		virtual ~Shape(){}
+		
 		virtual HitRecord intersect(const Ray &ray) = 0;  
 	};
 }
